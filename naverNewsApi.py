6c688617660c1e44ad904b030399137b2f0d0e2e
gyojincompany <luckyred@gmail.com>
2024-11-26 21:31:28 +0900

    버그 수정 및 최종결과 파일 출력 추가

--- naverNewsApi.py
+++ naverNewsApi.py
@@ -2,9 +2,12 @@ import urllib.request
 import datetime
 import json
 
-client_id = ""
-client_secret = ""
+client_id = "oNkjxMAROssEBw5enKkU"
+client_secret = "e98SgRiuR2"
+
+
 # 해당 url에 대한 접속 요청 후 정상적인 응답을 확인해서 결과를 반환해주는 함수
+jsonResult = []
 def getRequestUrl(url):
     req = urllib.request.Request(url)
     req.add_header("X-Naver-Client-Id", client_id)
@@ -40,6 +43,68 @@ def getNaverSearch(node, srcText, start, display):
     else:  # 응답 성공 
         return json.loads(responseDecode)  # 파이썬에서 사용가능한 객체로 변환해서 반환
 
-print(getNaverSearch("news","프로야구","1","20"))
+# print(getNaverSearch("news","프로야구","1","20"))
+
+
+
+def getPostData(post, jsonResult, cnt):
+    title = post["title"]  # 뉴스 제목
+    description = post["description"]  # 뉴스 요약
+    org_link = post["originallink"]  # 뉴스 기사의 원본 신문사 링크
+    link = post["link"]  # 네이버에서 링크한 기사 링크
+    # post["pubDate"]
+    pDate = datetime.datetime.strptime(post["pubDate"], "%a, %d %b %Y %H:%M:%S +0900")
+    # 원본 시간형식 'Wed, 19 Jul 2023 17:24:00 +0900' 에서 각 값을 가져오기
+    pDate = pDate.strftime("%Y-%m-%d %H:%M:%S")   # 2024-11-26 10:29:37 형식으로 변환
+    jsonResult.append({"cnt":cnt, "title":title, "description":description, "org_link":org_link, "link":link, "pDate":pDate})
+
+    return jsonResult
+
+node = "news"  # 검색할 카테고리
+srcText = "프로야구"  # 검색어
+
+# news 카테고리에서 "프로야구" 검색어로 1번부터 100번까지 100개의 검색 결과를 가져옴
+jsonResponse = getNaverSearch(node, srcText, "1", "100")
+# jsonResponse = getNaverSearch(node, srcText, 101, 100) True
+# jsonResponse = getNaverSearch(node, srcText, 201, 100) True
+# jsonResponse = getNaverSearch(node, srcText, 301, 100) True  -> total이 330개라 가정하면 마지막 30개의 결과만 들어있음
+# jsonResponse = getNaverSearch(node, srcText, 401, 100) False -> None 값으로 반환
+
+
+total = jsonResponse["total"]  # 검색된 뉴스 기사의 총 개수
+resultList = []  # 최종 결과가 들어갈 빈 리스트 선언
+cnt = 0
+
+while (jsonResponse != None) and (jsonResponse["display"] != 0):
+    for post in jsonResponse["items"]:
+        cnt = cnt + 1
+        getPostData(post, resultList, cnt)
+
+    start = jsonResponse["start"] + jsonResponse["display"]
+    jsonResponse = getNaverSearch(node, srcText, start, "100")
+
+
+print(f"검색된 총 뉴스 기사 수 : {total}건")
+
+with open(f"{srcText}_naver_{node}.json","w",encoding="utf-8") as outfile:
+    jsonFile = json.dumps(resultList, indent=4, sort_keys=True, ensure_ascii=False)
+    outfile.write(jsonFile)
+
+print(f"네이버 Api에서 가져온 총 뉴스 데이터 수 : {cnt}")
+
+## 파일 불러오기 참고
+# f = open("abc.txt","w")
+# f.write("안녕하세요")
+# f.close()
+#
+# with open("abc.txt","w") as f:
+#     f.write("안녕하세요")
+
+
+
+
+
+
+
 
 
